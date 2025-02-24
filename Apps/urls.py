b439999aeea3bf176f71f4d8131e9a4774562793
Ismai21k <ismailkabir170@gmail.com>
2025-02-24 18:50:08 +0100

    Add migrations

--- Apps/urls.py
+++ Apps/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [path('', views.test, name='test'),
                path('you/', views.youtube, name='youtube'),
+               path('reg/', views.user_registration, name='register'),
+               path('login/', views.user_login, name='user_login'),
+               path('logout/', views.user_logout, name='user_logout'),
+               
                
                ] 
 
--- Apps/views.py
+++ Apps/views.py
@@ -1,16 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout, authenticate
+from .forms import CustomUserCreationForm
 import yt_dlp
 import os
 
 def test(request):
     return HttpResponse("Hello World! ")
 
-from django.shortcuts import render
-from django.http import HttpResponse
-import yt_dlp
-import os
+def user_registration(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.email = form.cleaned_data.get('email')
+            user.save()
+            messages.success(request, "Registration successfull")
+            return redirect('youtube')
+        
+        else:    
+            messages.error(request, 'Invalid credentials')
+        
+    else:
+        form = CustomUserCreationForm() 
+    return render(request, 'Apps/register.html',{'form':form})
 
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "login successful!")
+                return redirect('youtube')
+            else:
+                messages.error(request,"Invalid Username or password")
+        
+        else:
+            messages.error(request,"Invalid Username or password")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'Apps/login.html',{'form':form})
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "logged out successfully!")
+    return redirect('user_login')
+
+ 
 def youtube(request):
     video_url = None
     error_message = None
