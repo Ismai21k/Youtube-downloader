33bba89c6d9b5d03414f26bbe61515d0cc7ead27
Ismai21k <ismailkabir170@gmail.com>
2025-03-08 18:46:49 +0100

    Refactor CustomUserCreationForm to simplify field

--- Apps/forms.py
+++ Apps/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=50, required=True, help_text="Enter your first name")
-    last_name = forms.CharField(max_length=50, required=True, help_text="Enter your last name")
-    email = forms.EmailField(required=True, help_text="Enter your Email address")
+    first_name = forms.CharField(max_length=50, required=True )
+    last_name = forms.CharField(max_length=50, required=True )
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
