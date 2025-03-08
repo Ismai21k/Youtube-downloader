230cbea6c0a0d33de2b4c4e8ebe15b4e12e07a4b
Ismai21k <ismailkabir170@gmail.com>
2025-03-08 21:16:34 +0100

    Update settings.py to configure static and media file paths, and add email backend settings

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -13,15 +13,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-STATIC_URL = '/static/'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Apps/static/Apps'),  # Adjust path if needed
+]
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Apps/downloads/')
-'''STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Apps/static'),  # Adjust path if needed
-]'''
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'Apps/downloads/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -129,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '123456@gmail.com'  # Your Gmail
+EMAIL_HOST_PASSWORD = 'password'  # Use App Password (Not your real password!)
