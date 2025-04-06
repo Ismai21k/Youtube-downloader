8da68aa712bdf5b389a7b8e0f6659704be965fd8
Ismai21k <ismailkabir170@gmail.com>
2025-04-06 11:57:28 +0100

    media file

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '123456@gmail.com'  # Your Gmail
-EMAIL_HOST_PASSWORD = 'password'  # Use App Password (Not your real password!)
+EMAIL_HOST_USER = 'ismailkabir170@gmail.com'  # Your Gmail
+EMAIL_HOST_PASSWORD = 'mydp mnrs ploz csno'  # Use App Password (Not your real password!)
